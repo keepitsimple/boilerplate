392fa44758182d04a2ca1eb244e3d383378e1dce
Roman Podlinov <rpodlinov@gmail.com>
2021-06-19 18:57:21 +0300

    update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,5 +24,10 @@ module.exports = {
     'react'
   ],
   rules: {
+    "max-lines": [ "warn", 300],
+    "max-lines-per-function": [ "warn", 30],
+    "complexity": [ "warn", 5],
+    "max-params": [ "warn", 3]
+
   }
 }
